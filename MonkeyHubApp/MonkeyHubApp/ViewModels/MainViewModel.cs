43c4b07a6fbabbd5d3b9908db634621777affe3f
Aarão Pedro Pires de Medeiros Junior <aaraojunior@mpms.mp.br>
2017-05-09 18:09:51 -0400

    Primeiro Commit

---
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace MonkeyHubApp.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+        private string _searchTerm;
+
+        public string SearchTerm
+        {
+            get { return _searchTerm; }
+            set { SetProperty(ref _searchTerm, value); }
+        }
+
+        public Command SearchCommand { get; }
+
+        public MainViewModel()
+        {
+            SearchCommand = new Command(ExecuteSearchCommand);
+        }
+
+        void ExecuteSearchCommand()
+        {
+            Debug.WriteLine();
+        }
+    }
+}
